d19046de92ccfc60f18175130a8401c406f6beac
activo5408-blip <activo5408@gmail.com>
2026-09-24 11:36:06 -0400

    Confirmar y seguir

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.negocio.libromayor',
-  appName: 'Libro Mayor',
-  webDir: 'www'
-};
-
-export default config;
